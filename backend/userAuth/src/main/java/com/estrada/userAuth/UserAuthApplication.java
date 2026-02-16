3d792ec71793e250304b5443ee6be4dca231706e
carmel-estrada28 <carmelestradaa@gmail.com>
2026-02-16 21:40:05 +0800

    setup(main): setup

--- backend/userAuth/src/main/java/com/estrada/userAuth/UserAuthApplication.java
+++ backend/userAuth/src/main/java/com/estrada/userAuth/UserAuthApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UserAuthApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(UserAuthApplication.class, args);
 	}
 
---
+++ backend/userAuth/src/main/java/com/estrada/userAuth/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.estrada.userAuth.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping("/api/test")
+public class TestController {
+
+    @GetMapping
+    public String test() {
+        return "Backend is working!";
+    }
+}
